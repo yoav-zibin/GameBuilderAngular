c032bf9f36205719b033a28ac7223948e34ec8d7
odoric27 <dmo294@nyu.edu>
2017-09-27 13:40:53 -0400

    refactor dir structure

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,6 +1,6 @@
 import { AppPage } from './app.po';
 
-describe('gamebuilder App', () => {
+describe('GameBuilderAngular App', () => {
   let page: AppPage;
 
   beforeEach(() => {
@@ -9,6 +9,6 @@ describe('gamebuilder App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getParagraphText()).toEqual('Welcome to GameBuilder!');
   });
 });
