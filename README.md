6a9e232c5c508ae1801f246ba3199cab70c01713
odoric27 <dmo294@nyu.edu>
2017-09-27 13:26:33 -0400

    Auto-generated commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# GameBuilderAngular
-GameBuilderAngular
--- gamebuilder/src/app/window.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@angular/core';
-
-@Injectable()
-export class WindowService {
-    get windowRef() {
-        return window
-    }
-}
