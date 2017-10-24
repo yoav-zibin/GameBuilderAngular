0f2881e0ce519f7321432dc0496b1d8bda717c49
jockyxu1122 <yx1366@nyu.edu>
2017-10-23 22:08:07 -0400

    Added 'cards deck' and 'pieces deck'

--- src/app/create-element/create-element.component.ts
+++ src/app/create-element/create-element.component.ts
@@ -26,17 +26,27 @@ enum Types {
 export class CreateElementComponent implements OnInit {
   basePath: string = "gameBuilder/elements/";
   elementCreated: boolean = false;
+  elementInfo: Object = {};
   elementType: string;
   faceNumber: number;
-  hideSubmitButton: boolean = true;
-  images: any;
   isDraggable: boolean = false;
   isDrawable: boolean = false;
   rotatableDegrees: number = 360;
+
+  // Images and elements(cards).
+  elements: any;
+  images: any;
+  selectedElements: any = [];
   selectedImages: any = [];
 
-  userIsAnonymous: boolean;
+  // Visability control.
+  hideNextButton: boolean = true;
+  hideSubmitButton: boolean = true;
+  showElements: boolean = false;
+  
+  // For auth use.
   userEmailEmpty: boolean;
+  userIsAnonymous: boolean;
 
   constructor(
     private imageSelectionService: ImageSelectionService,
@@ -56,21 +66,7 @@ export class CreateElementComponent implements OnInit {
     }
   }
 
-  select(image) {
-    if (this.selectedImages.length < this.faceNumber && this.selectedImages.indexOf(image) == -1) {
-      this.selectedImages.push(image);
-    } else if (this.selectedImages.length <= this.faceNumber && this.selectedImages.indexOf(image) != -1) {
-      this.selectedImages.splice(this.selectedImages.indexOf(image), 1);
-    }
-
-    this.validateImages();
-  }
-
-  reset() {
-    this.elementCreated = false;
-  }
-
-  submit() {
+  next() {
     if (this.rotatableDegrees < 1 || this.rotatableDegrees > 360) {
       window.alert("Rotatable degrees must fall in range 1 ~ 360");
       return;
@@ -82,34 +78,82 @@ export class CreateElementComponent implements OnInit {
           "imageId": image.$key
         };
     });
+    Object.assign(this.elementInfo, this.getBasicElementInfo(), {"images": images});
 
-    let elementInfo = {};
-    Object.assign(elementInfo, this.getBasicElementInfo(), {"images": images});
-    console.log(elementInfo);
+    if (!this.isDeck()) {
+      this.submit();
+    } else {
+      this.elements = this.imageSelectionService.getElements();
+      this.showElements = true;
+    }
+  }
+
+  reset() {
+    this.elementCreated = false;
+    this.elementInfo = {};
+    this.elementType = "";
+    this.faceNumber = null;
+    this.isDraggable = false;
+    this.isDrawable = false;
+    this.rotatableDegrees = 360;
+  
+    // Images and elements(cards).
+    this.selectedElements = [];
+    this.selectedImages = [];
+  
+    // Visability control.
+    this.hideNextButton = true;
+    this.hideSubmitButton = true;
+    this.showElements = false;
+  }
+
+  selectElement(element) {
+    let elementId: string = element.$key;
+    if (this.selectedElements.indexOf(elementId) == -1) {
+      this.selectedElements.push(elementId);
+    } else {
+      this.selectedElements.splice(this.selectedElements.indexOf(elementId), 1);
+    }
+    this.validateElements();
+  }
+
+  selectImage(image) {
+    if (this.selectedImages.length < this.faceNumber && this.selectedImages.indexOf(image) == -1) {
+      this.selectedImages.push(image);
+    } else if (this.selectedImages.length <= this.faceNumber && this.selectedImages.indexOf(image) != -1) {
+      this.selectedImages.splice(this.selectedImages.indexOf(image), 1);
+    }
+    this.validateImages();
+  }
+
+  submit() {
+    if (this.isDeck()) {
+      let deckInfo = {};
+      this.selectedElements.forEach((elementId, index) => {
+        deckInfo[String(index)] = 
+          {
+            "deckMemberElementId": elementId
+          };
+      });
+
+      let deckElementInfo: Object = {};
+      Object.assign(deckElementInfo, this.elementInfo, {"deckElements": deckInfo});
+      this.elementInfo = deckElementInfo;
+    }
+
     const elementId: string = this.af.database.ref(this.basePath).push().key;
-    this.af.database.ref(`${this.basePath}${elementId}`).update(elementInfo);
-
-    this.clearArray();
+    this.af.database.ref(`${this.basePath}${elementId}`).update(this.elementInfo);
     this.elementCreated = true;
   }
 
   updateFaceNumber(i) {
     this.faceNumber = i;
-    this.clearArray();
+    this.selectedImages = [];
     this.validateImages();
   }
 
-  private validateImages() {
-    if (this.faceNumber == this.selectedImages.length) {
-      let image = this.selectedImages[0];
-      let height: number = image.height;
-      let width: number = image.width;
-      this.hideSubmitButton = !this.selectedImages.every(function sameHeightWidth(element, index, array) {
-        return element.height == height && element.width == width;
-      });
-    } else {
-      this.hideSubmitButton = true;
-    }
+  private isDeck() {
+    return this.elementType == Types[Types.cardsDeck] || this.elementType == Types[Types.piecesDeck];
   }
 
   private getBasicElementInfo() {
@@ -126,24 +170,21 @@ export class CreateElementComponent implements OnInit {
     }
   }
 
-  private clearArray() {
-    this.selectedImages = [];
+  private validateElements() {
+    this.hideSubmitButton = this.selectedElements.length < 2;
+  }
+
+  private validateImages() {
+    if (this.faceNumber == this.selectedImages.length) {
+      let image = this.selectedImages[0];
+      let height: number = image.height;
+      let width: number = image.width;
+      this.hideNextButton = !this.selectedImages.every(function sameHeightWidth(element, index, array) {
+        return element.height == height && element.width == width;
+      });
+    } else {
+      this.hideNextButton = true;
+    }
   }
 
 }
-
-/*
-newData.hasChildren(['uploaderEmail', 'uploaderUid', 'createdOn', 'width', 'height', 'isDraggable', 'elementKind', 'rotatableDegrees', 'isDrawable']) &&
-$elementId.matches(/^[-_A-Za-z0-9]{17,40}$/) && 
-(
-  (newData.child('elementKind').val().matches(/^cardsDeck|piecesDeck$/) ? newData.child('deckElements/1').exists() : !newData.child('deckElements').exists())  && 
-  (newData.child('isDrawable').val() === false || newData.child('elementKind').val().matches(/^standard|card$/))  && 
-  (newData.child('rotatableDegrees').val() === 360 || newData.child('elementKind').val() === 'standard')  && 
-  newData.child('images/0').exists()  && 
-  (
-    (newData.child('elementKind').val().matches(/^standard|cardsDeck|piecesDeck$/) && !newData.child('images/1').exists())  || 
-    (newData.child('elementKind').val().matches(/^toggable|dice$/) && newData.child('images/1').exists())  || 
-    (newData.child('elementKind').val() === 'card' && newData.child('images/1').exists() && !newData.child('images/2').exists()) 
-  )
-)
-*/
--- src/app/create-element/imageSelection.service.ts
+++ src/app/create-element/imageSelection.service.ts
@@ -8,9 +8,19 @@ export class ImageSelectionService {
     getImages() {
         let query = {
             orderByKey: true,
-        }
+        };
         return this.db.list('gameBuilder/images', {
             query
-        })        
+        }); 
+    }
+
+    getElements() {
+        let query = {
+            orderByChild: "elementKind",
+            equalTo: "card"
+        };
+        return this.db.list('gameBuilder/elements', {
+            query
+        });
     }
 }
